7b45e71746e045fa39334e082209ce6e5db325b2
MaezarAbdillah <113416817+MaezarAbdillah@users.noreply.github.com>
2022-10-20 22:13:49 +0800

    Delete POSTTEST_5 directory

--- POSTTEST_5/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $localhost = "localhost";
-    $user = "root";
-    $password = "";
-    $db_name = "db_spot";
-    
-    $conn = mysqli_connect($localhost, $user, $password, $db_name);
-?>
